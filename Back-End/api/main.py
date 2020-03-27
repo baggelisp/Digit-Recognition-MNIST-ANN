e8eeb95237169382195fe2ec70bf8c361df74201
Vangelis Spathonis <baggelisp.kef@hotmail.com>
2020-03-27 18:11:05 +0200

    post request

--- Back-End/api/main.py
+++ Back-End/api/main.py
@@ -1,11 +1,21 @@
-import flask
+from flask import Flask,request,jsonify
+from flask_cors import CORS
 
-app = flask.Flask(__name__)
+
+app = Flask(__name__)
+CORS(app)
 app.config["DEBUG"] = True
 
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return 'Get Request'
+    
+@app.route('/', methods=['POST'])
+def canvasArray():
+    data = jsonify(request.json)
+    print (data)
+    return data
+
 
 app.run()
