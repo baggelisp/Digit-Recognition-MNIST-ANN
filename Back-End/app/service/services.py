86e9ba735c25c44d2a30714991d8885a4b6626f9
Vangelis Spathonis <baggelisp.kef@hotmail.com>
2020-04-01 14:30:28 +0300

    path to model

--- Back-End/app/service/services.py
+++ Back-End/app/service/services.py
@@ -20,7 +20,7 @@ def predictDigitNumber(imageToPredict):
      Dense(64, activation='relu'),
      Dense(10, activation='softmax'),
     ])
-    # model.load_weights('app/model/model.h5') # running on localhost
+    # model.load_weights('model/model.h5') # running on localhost
     model.load_weights('app/model/model.h5') # running with docker
     imageToPredict = np.array([imageToPredict])
     # Make prediction
